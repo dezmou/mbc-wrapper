00f8ddd091f3375f3fb0ed2af0427f0e367f38fa
Dezmou <incredibledez@gmail.com>
2023-02-13 14:40:43 +0100

    wip

--- contracts/WrappedMyBlockchainCorner.sol
+++ contracts/WrappedMyBlockchainCorner.sol
@@ -139,16 +139,91 @@ contract WrappedMyBlockchainCorner is
         uint256 tokenId
     ) public view override(ERC721) returns (string memory) {
         ownerOf(tokenId);
-        uint256[3] memory coords = tokenIdToCoordinate[tokenId];
+        // uint256[3] memory coords = tokenIdToCoordinate[tokenId];
 
-        return
-            string.concat(
-                "Page : ",
-                Strings.toString(coords[0]),
-                " x : ",
-                Strings.toString(coords[1]),
-                " y : ",
-                Strings.toString(coords[2])
-            );
+        string memory image = '<svg width="400" height="110"><rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" /></svg>';
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "chien", "description": "c le chien et tout", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(image)), '"}'))));
+        return string(abi.encodePacked('data:application/json;base64,', json));
+
+        // return
+        //     string.concat(
+        //         "Page : ",
+        //         Strings.toString(coords[0]),
+        //         " x : ",
+        //         Strings.toString(coords[1]),
+        //         " y : ",
+        //         Strings.toString(coords[2])
+        //     );
+    }
+}
+
+/// [MIT License]
+/// @title Base64
+/// @notice Provides a function for encoding some bytes in base64
+/// @author Brecht Devos <brecht@loopring.org>
+library Base64 {
+    bytes internal constant TABLE =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    /// @notice Encodes some bytes to the base64 representation
+    function encode(bytes memory data) internal pure returns (string memory) {
+        uint256 len = data.length;
+        if (len == 0) return "";
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((len + 2) / 3);
+
+        // Add some extra buffer at the end
+        bytes memory result = new bytes(encodedLen + 32);
+
+        bytes memory table = TABLE;
+
+        assembly {
+            let tablePtr := add(table, 1)
+            let resultPtr := add(result, 32)
+
+            for {
+                let i := 0
+            } lt(i, len) {
+
+            } {
+                i := add(i, 3)
+                let input := and(mload(add(data, i)), 0xffffff)
+
+                let out := mload(add(tablePtr, and(shr(18, input), 0x3F)))
+                out := shl(8, out)
+                out := add(
+                    out,
+                    and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF)
+                )
+                out := shl(8, out)
+                out := add(
+                    out,
+                    and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF)
+                )
+                out := shl(8, out)
+                out := add(
+                    out,
+                    and(mload(add(tablePtr, and(input, 0x3F))), 0xFF)
+                )
+                out := shl(224, out)
+
+                mstore(resultPtr, out)
+
+                resultPtr := add(resultPtr, 4)
+            }
+
+            switch mod(len, 3)
+            case 1 {
+                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
+            }
+            case 2 {
+                mstore(sub(resultPtr, 1), shl(248, 0x3d))
+            }
+
+            mstore(result, encodedLen)
+        }
+
+        return string(result);
     }
 }
